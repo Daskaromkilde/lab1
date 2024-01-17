957d7a1da8c4eb20e4ddcda6a4fc1937a4d9e31d
Damkilde <oskardamkilde@gmail.com>
2024-01-17 13:03:38 +0100

    Skapat car class. Extendat saab&volvo

---
+++ Car.java
@@ -0,0 +1,62 @@
+import java.awt.*; //?? color
+
+
+public class Car {
+
+    public int nrDoors; // Number of doors on the car                           BÅDA HAR
+    public double enginePower; // Engine power of the car                       BÅDA HAR
+    public double currentSpeed; // The current speed of the car                 BÅDA HAR
+    public Color color; // Color of the car                                     BÅDA HAR
+    public String modelName; // The car model name
+
+
+
+}
+
+
+
+
+
+
+
+/*
+public class Volvo240{
+
+    public final static double trimFactor = 1.25;
+    public int nrDoors; // Number of doors on the car                           BÅDA HAR
+    public double enginePower; // Engine power of the car                       BÅDA HAR
+    public double currentSpeed; // The current speed of the car                 BÅDA HAR
+    public Color color; // Color of the car                                     BÅDA HAR
+    public String modelName; // The car model name                              BÅDA HAR
+
+    public Volvo240(){
+        nrDoors = 4;
+        color = Color.black;
+        enginePower = 100;
+        modelName = "Volvo240";
+        stopEngine();
+    }
+ */
+
+
+
+/*
+public class Saab95{
+
+    public boolean turboOn;
+    public int nrDoors; // Number of doors on the car
+    public double enginePower; // Engine power of the car
+    public double currentSpeed; // The current speed of the car
+    public Color color; // Color of the car
+    public String modelName; // The car model name
+
+    public Saab95(){
+        nrDoors = 2;
+        color = Color.red;
+        enginePower = 125;
+	    turboOn = false;
+        modelName = "Saab95";
+        stopEngine();
+    }
+
+ */
