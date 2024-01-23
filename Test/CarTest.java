c657e3c4cfd16c11ed515bb284c6fa7e1e8a86aa
Damkilde <oskardamkilde@gmail.com>
2024-01-23 14:44:45 +0100

    added all test cases

--- Test/CarTest.java
+++ Test/CarTest.java
@@ -1,32 +1,65 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
+    static Saab95 saab = new Saab95();
 
-    @Test
-    void performAction() {
+    static Volvo240 volvo = new Volvo240();
+    @BeforeAll
+    public static void init() {
+        saab = new Saab95();
+        volvo = new Volvo240();
+
     }
 
-    @Test
+
+    @Test //
     void getPosition() {
+        Point pos = new Point(50,100);
+        saab.setPosition(pos);
+        assertEquals(new Point(50,100), saab.getPosition());
+
     }
 
-    @Test
+    @Test //
     void setPosition() {
+        Point pos = new Point(550,100);
+        Point newPost = new Point(100, -50);
+        saab.setPosition(pos);
+        assertEquals(new Point(550,100), saab.getPosition());
+        saab.setPosition(newPost);
+        assertEquals(new Point(100, -50), saab.getPosition());
     }
 
-    @Test
+    @Test //
     void getModelName() {
+        volvo.setModelName("MinFinaVolvo");
+        assertEquals("MinFinaVolvo", volvo.getModelName());
+        String volvoFul = "MinFulaVolvo";
+        String volvoFin = volvo.getModelName();
+        assertFalse(Boolean.parseBoolean(volvoFul), volvoFin);
     }
 
     @Test
     void getTurbo() {
+        saab.setTurboOff();
+        assertFalse(saab.getTurbo(),  "Turbo should be off by default");
+
+        saab.setTurboOn();
+        assertTrue(saab.getTurbo(), "Turbo should be on after setting it on");
+
+        saab.setTurboOff();
+        assertFalse(saab.getTurbo(), "Turbo should be off after setting it off");
     }
 
     @Test
     void setCurrentSpeed() {
+        volvo.setCurrentSpeed(50);
+        assertEquals(50, volvo.getCurrentSpeed(), "Current speed should be updated correctly");
     }
 
     @Test
@@ -43,18 +76,32 @@ class CarTest {
 
     @Test
     void getNrDoors() {
+        volvo.setNrDoors(2);
+        assertEquals(2, volvo.getNrDoors());
+        volvo.setNrDoors(4);
+        assertEquals(4, volvo.getNrDoors());
     }
 
     @Test
     void getEnginePower() {
+        volvo.setEnginePower(200);
+        assertEquals(200, volvo.getEnginePower(), "Engine power should be updated correctly");
+
+        volvo.setEnginePower(100);
+        assertEquals(100, volvo.getEnginePower(), "Engine power should be updated correctly");
     }
 
     @Test
     void getCurrentSpeed() {
+        volvo.setCurrentSpeed(50.0);
+        assertEquals(50.0, volvo.getCurrentSpeed());
     }
 
     @Test
     void getColor() {
+        volvo.setColor(Color.BLUE);
+        assertEquals(Color.BLUE, volvo.getColor(), "Color should be updated correctly");
+
     }
 
     @Test
@@ -63,57 +110,95 @@ class CarTest {
 
     @Test
     void startEngine() {
+        saab.startEngine();
+        assertEquals(0.1, saab.getCurrentSpeed(), "Current speed should be set correctly when starting engine");
     }
 
     @Test
     void stopEngine() {
+        volvo.stopEngine();
+        assertEquals(0.0, volvo.getCurrentSpeed(), "Current speed should be 0 when stopping engine");
     }
 
     @Test
     void setTurboOn() {
+        saab.setTurboOn();
+        assertTrue(saab.getTurbo(), "Turbo should be on");
+
     }
 
     @Test
     void setTurboOff() {
+        saab.setTurboOff();
+        assertFalse(saab.getTurbo(), "Turbo should be off");
     }
 
     @Test
     void incrementSpeed() {
+        saab.setTurboOn();
+        saab.setCurrentSpeed(45.0);
+        saab.incrementSpeed(5.0);
+        assertEquals(53.125, saab.getCurrentSpeed());
+
+        volvo.setCurrentSpeed(32.0);
+        volvo.incrementSpeed(7.0);
+        assertEquals(40.75, volvo.getCurrentSpeed());
+
     }
+
 
     @Test
     void decrementSpeed() {
+        saab.setTurboOff();
+        saab.setCurrentSpeed(40.0);
+        saab.decrementSpeed(8.0);
+        assertEquals(30.0, saab.getCurrentSpeed());
+
+        volvo.setCurrentSpeed(60.0);
+        volvo.decrementSpeed(9.0);
+        assertEquals(48.75, volvo.getCurrentSpeed());
     }
 
     @Test
     void gas() {
+        volvo.setCurrentSpeed(50);
+        volvo.gas(0.7);
+        assertEquals(50.875, volvo.getCurrentSpeed());
+
     }
 
     @Test
     void brake() {
+        saab.setTurboOn();
+        saab.setCurrentSpeed(47);
+        saab.brake(0.8);
+        assertEquals(45.7, saab.getCurrentSpeed());
     }
 
     @Test
     void move() {
+        volvo.setCurrentSpeed(50);
+        volvo.setDirection(eDirection.SOUTH);
+        volvo.setPosition(new Point(100,50));
+        volvo.move(volvo.getDirection());
+        assertEquals(new Point(100,0), volvo.getPosition());
     }
 
     @Test
     void turnLeft() {
-        Volvo240 car = new Volvo240();
-        car.setDirection(eDirection.WEST);
+        volvo.setDirection(eDirection.WEST);
 
-        car.turnLeft();
+        volvo.turnLeft();
 
-        assertEquals(eDirection.SOUTH, car.getDirection(), "When west, result==south");
+        assertEquals(eDirection.SOUTH, volvo.getDirection(), "When west, result==south");
 
 
     }
 
     @Test
     void turnRight() {
-        Saab95 car = new Saab95();
-        car.setDirection(eDirection.NORTH);
-        car.turnRight();
-        assertEquals(eDirection.EAST, car.getDirection(), "When north, result==east");
+        saab.setDirection(eDirection.NORTH);
+        saab.turnRight();
+        assertEquals(eDirection.EAST, saab.getDirection(), "When north, result==east");
     }
 }
--- Volvo240.java
+++ Volvo240.java
@@ -3,6 +3,8 @@ import java.awt.*;
 public class Volvo240 extends Car{
     final static double trimFactor = 1.25;
 
+
+
     public Volvo240(){
         super(4, 100, Color.black, "Volvo240");
     }
