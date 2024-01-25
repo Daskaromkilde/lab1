2f226219610539c712d2af3219a456d2642042de
Simon <64027803+simonsimme@users.noreply.github.com>
2024-01-25 15:42:43 +0100

    Changes by johanbrynne

--- Workshop.java
+++ Workshop.java
@@ -6,15 +6,23 @@ import java.util.*;
 public class Workshop implements CarWorkshop {
 
     private int maxCapacity;
+    private List<Car> carInWorkshop;
 
 
     public Workshop(int maxCapacity) {
         this.maxCapacity = maxCapacity;
+        this.carInWorkshop = new ArrayList<>();
 
     }
 
-    public void acceptCar(){
+    public void repairCar(Car car){
     }
+
+    public void loadCar(Car car){
+
+    }
+
+
 
 
 
