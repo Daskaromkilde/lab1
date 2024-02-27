e9faa709f4b541774da1d01fc755c82595357073
Damkilde <oskardamkilde@gmail.com>
2024-02-27 15:04:17 +0100

    Changes by johanbrynne

--- MVC/Observable.java
+++ MVC/Observable.java
@@ -1,19 +1,23 @@
 package MVC;
 
 import java.util.ArrayList;
+import java.util.Observer;
 
 public class Observable {
 
-    private ArrayList<Observer> observers = new ArrayList<>();
+    private ArrayList<iObserver> observers = new ArrayList<>();
 
-    public void addObservers(Observer observer){
-        observer.add(observer);
+    public void addObservers(iObserver observer){
+        observers.add(observer);
     }
     public void removeObservers(iObserver observer){
+        observers.remove(observer);
 
     }
     public void notifyObservers() {
-
+        //System.out.println(observers.size());
+        for (iObserver observer : observers)
+            observer.notifyOb();
     }
 
 }
